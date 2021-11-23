f456d501a8f160191d375083e784bff43f16107d
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-11-22 23:54:14 -0500

    work

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist/BankProject'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/src/index.js'));});
-app.listen(process.env.PORT || 4200);
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
+});
