ed03672aa26ca0552b8dfed633716f3633aebdc9
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-11-22 23:46:39 -0500

    work

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/BankProject'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/src/index.js'));});
+app.listen(process.env.PORT || 4200);
