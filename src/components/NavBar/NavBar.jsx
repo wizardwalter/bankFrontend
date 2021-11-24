b3ecf9e5ed44032f727d8b0f3217b4df5279fb24
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-11-23 22:41:47 -0500

    done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import './NavBar.css';
 import { AuthContext } from '../../AuthContext';
-import  {Navbar, Nav}  from 'react-bootstrap';
 
  
 const NavBar = () => {
@@ -18,17 +17,25 @@ const NavBar = () => {
     <>
     { user !== null ?
   <div className="row d-flex justify-content-around" data-testid="NavBar">
- <NavBar className="navbar navbar-expand-lg">
+ <nav className="navbar navbar-expand-lg">
     <div className="col-6 d-flex align-items-center justify-content-around">
     <Link className="navbar-brand" to="/"><img src="https://img.icons8.com/external-inipagistudio-mixed-inipagistudio/64/000000/external-bank-urban-infrastructure-inipagistudio-mixed-inipagistudio.png" alt="logo"/> Monroe Bank</Link>
-      <NavBar.Toggle className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"/>
-      <Navbar.Collapse className="collapse navbar-collapse" id="navbarNav">
-        <Nav>
-        <Nav.Link className="link" to="/deposit/">Deposit</Nav.Link>
-        <Nav.Link className="link" to="/withdraw/">Withdraw</Nav.Link>
-        <Nav.Link className="link" to="/alldata/">Account Balance</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="link" to="/deposit/">Deposit</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="link" to="/withdraw/">Withdraw</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="link" to="/alldata/">Account Balance</Link>
+          </li>
+          </ul>
+      </div>
     </div>
     <div className="col-6 d-flex align-items-center justify-content-end">
       <ul className="navbar-nav">
@@ -40,20 +47,26 @@ const NavBar = () => {
       </span>
       </ul>
       </div>
-  </NavBar>
+  </nav>
   </div>
       :
       <div className="NavBar" data-testid="NavBar">
-      <NavBar className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/"><img src="https://img.icons8.com/external-inipagistudio-mixed-inipagistudio/64/000000/external-bank-urban-infrastructure-inipagistudio-mixed-inipagistudio.png" alt="logo"/> Monroe Bank</Link>
-      <NavBar.Toggle className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"/>
-        <Navbar.Collapse className="collapse navbar-collapse" id="navbarNav">
-          <Nav>
-            <Nav.Link className="link" to="/CreateAccount/">Create Account</Nav.Link>
-            <Nav.Link className="link" to="/login/">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </NavBar>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="link" href="/CreateAccount/">Create Account</a>
+            </li>
+            <li className="nav-item">
+              <a className="link" href="/login/">Login</a>
+            </li>        
+          </ul>
+        </div>
+      </nav>
     </div>
       } 
     </>
